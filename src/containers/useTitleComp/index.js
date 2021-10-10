bf06fb8683ab54ac7dbcd1b3940dba153297140c
Moon-sang ho <skym887@gmail.com>
2021-10-10 22:21:32 +0900

    refactor: apply typescript to useTitle

--- src/containers/useTitleComp/index.js
+++
@@ -1,9 +0,0 @@
-import useTitle from "../../hooks/useTitle";
-
-const UseTitleComp = () => {
-  const titleUpdator = useTitle("Loading...");
-  setTimeout(() => titleUpdator("Home"), 3000);
-  return <div>see the web-page's title. it will change the title.</div>;
-};
-
-export default UseTitleComp;
