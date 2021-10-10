763d5e81dfede28f85e602a51d13d7401c628d56
Moon-sang ho <skym887@gmail.com>
2021-10-10 22:03:37 +0900

    refactor: apply typescript to useTabs

--- src/containers/useTabComp/index.tsx
+++ src/containers/useTabComp/index.tsx
@@ -1,26 +1,26 @@
-import useTabs from "../../hooks/useTabs";
+import useTabs from '../../hooks/useTabs';
 
-const content = [
+const contents = [
   {
-    tab: "Section 1",
+    tab: 'Section 1',
     content: "I'm the content of the Section 1",
   },
   {
-    tab: "Section 2",
+    tab: 'Section 2',
     content: "I'm the content of the Section 2",
   },
   {
-    tab: "Section 3",
+    tab: 'Section 3',
     content: "I'm the content of the Section 3",
   },
 ];
 
 const UseTabComp = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const { currentItem, changeItem } = useTabs(0, contents);
 
   return (
     <div>
-      {content.map((section, index) => (
+      {contents.map((section, index) => (
         <button key={index} onClick={() => changeItem(index)}>
           {section.tab}
         </button>
