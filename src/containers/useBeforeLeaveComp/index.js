32dd1c3927b87fdd4a66b00f95cd387984a3b905
Moon-sang ho <skym887@gmail.com>
2021-10-11 19:15:55 +0900

    refactor: apply typescript to useBeforeLeave

--- src/containers/useBeforeLeaveComp/index.js
+++
@@ -1,9 +0,0 @@
-import useBeforeLeave from "../../hooks/useBeforeLeave";
-
-const UseBeforeLeaveComp = () => {
-  const begForNotLeave = () => console.log("Pls dont leave");
-  useBeforeLeave(begForNotLeave);
-  return <div>hello</div>;
-};
-
-export default UseBeforeLeaveComp;
