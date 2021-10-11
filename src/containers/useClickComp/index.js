da6f26348406841872e202b83d4f2ffdd09bce89
Moon-sang ho <skym887@gmail.com>
2021-10-11 18:46:00 +0900

    refactor: apply typescript to useClick

--- src/containers/useClickComp/index.js
+++
@@ -1,9 +0,0 @@
-import useClick from "../../hooks/useClick";
-
-const UseClickComp = () => {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
-  return <h1 ref={title}>hello</h1>;
-};
-
-export default UseClickComp;
