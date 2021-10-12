ee00ed3f10c33b0c8a0d68289d65d29895d8a164
Moon-sang ho <skym887@gmail.com>
2021-10-12 19:50:27 +0900

    refactor: apply typescript to useScroll

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const useScroll = () => {
   const [state, setState] = useState({
@@ -10,9 +10,9 @@ const useScroll = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll);
 
-    return () => window.removeEventListener("scroll", onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
   return state;
