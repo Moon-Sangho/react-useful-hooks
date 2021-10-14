88b8b1ebea9a5bdf9143f9b140265092bf5c4184
Moon-sang ho <skym887@gmail.com>
2021-10-15 00:19:03 +0900

    feat: add navigation bar

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -12,7 +12,8 @@ export default Home;
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Title = styled.div`
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,28 +13,94 @@ import UseFullScreenComp from '../containers/useFullScreenComp';
 import UseNotificationComp from '../containers/useNotificationComp';
 import UseAxiosComp from '../containers/useAxiosComp';
 import Home from '../containers/Home';
+import styled from '@emotion/styled';
+import { useHistory } from 'react-router';
+
+const NavigationBar = () => {
+  const history = useHistory();
+
+  const routesArray = [
+    'Home',
+    'useInput',
+    'useTab',
+    'useTitle',
+    'useClick',
+    'usePreventLeave',
+    'useBeforeLeave',
+    'useFadeIn',
+    'useNetwork',
+    'useScroll',
+    'useFullScreen',
+    'useNotification',
+    'useAxios',
+  ];
+
+  return (
+    <div>
+      {routesArray.map((item) => {
+        let path = item;
+
+        if (item === 'Home') {
+          path = '';
+        }
+
+        return <Button onClick={() => history.push(`/${path}`)}>{item}</Button>;
+      })}
+    </div>
+  );
+};
 
 const Routes = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/useInput" component={UseInputComp} />
-        <Route exact path="/useTab" component={UseTabComp} />
-        <Route exact path="/useTitle" component={UseTitleComp} />
-        <Route exact path="/useClick" component={UseClickComp} />
-        <Route exact path="/useConfirm" component={UseConfirmComp} />
-        <Route exact path="/usePreventLeave" component={UsePreventLeaveComp} />
-        <Route exact path="/useBeforeLeave" component={UseBeforeLeaveComp} />
-        <Route exact path="/useFadeIn" component={UseFadeInComp} />
-        <Route exact path="/useNetwork" component={UseNetworkComp} />
-        <Route exact path="/useScroll" component={UseScrollComp} />
-        <Route exact path="/useFullScreen" component={UseFullScreenComp} />
-        <Route exact path="/useNotification" component={UseNotificationComp} />
-        <Route exact path="/useAxios" component={UseAxiosComp} />
-      </Switch>
+      <Container>
+        <NavigationBar />
+        <Main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/useInput" component={UseInputComp} />
+            <Route exact path="/useTab" component={UseTabComp} />
+            <Route exact path="/useTitle" component={UseTitleComp} />
+            <Route exact path="/useClick" component={UseClickComp} />
+            <Route exact path="/useConfirm" component={UseConfirmComp} />
+            <Route
+              exact
+              path="/usePreventLeave"
+              component={UsePreventLeaveComp}
+            />
+            <Route
+              exact
+              path="/useBeforeLeave"
+              component={UseBeforeLeaveComp}
+            />
+            <Route exact path="/useFadeIn" component={UseFadeInComp} />
+            <Route exact path="/useNetwork" component={UseNetworkComp} />
+            <Route exact path="/useScroll" component={UseScrollComp} />
+            <Route exact path="/useFullScreen" component={UseFullScreenComp} />
+            <Route
+              exact
+              path="/useNotification"
+              component={UseNotificationComp}
+            />
+            <Route exact path="/useAxios" component={UseAxiosComp} />
+          </Switch>
+        </Main>
+      </Container>
     </Router>
   );
 };
 
 export default Routes;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  margin: 10px 10px 0 0;
+`;
+
+const Main = styled.main`
+  padding-top: 20px;
+`;
