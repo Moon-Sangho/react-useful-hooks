c7920f673339170210e54a7f9af9ca5152af1e0f
Moon-sang ho <skym887@gmail.com>
2021-10-14 20:54:54 +0900

    feat: install emotion packages

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,9 +1,21 @@
+import styled from '@emotion/styled';
+
 const Home = () => {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <Container>
+      <Title>Home</Title>
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+  font-size: 28px;
+  margin-top: 20px;
+`;
